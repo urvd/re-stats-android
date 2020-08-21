85956af458dd446c58b68d27fad80e5fee5b0cef
uriel <uriel.vido@hotmail.fr>
2020-08-21 21:19:59 +0200

    mains activities

---
+++ app/src/main/java/com/example/re_stats_android/framents/authent/LoginViewModel.java
@@ -0,0 +1,7 @@
+package com.example.re_stats_android.framents.authent;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
